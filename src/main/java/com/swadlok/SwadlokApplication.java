26fd589acef8a759d924d76ee0b40d9728d753f2
ajitlol404 <ajitmicromax12@gmail.com>
2025-08-23 23:17:04 +0530

    Initial commit - Swadlok

---
+++ src/main/java/com/swadlok/SwadlokApplication.java
@@ -0,0 +1,13 @@
+package com.swadlok;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwadlokApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwadlokApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/swadlok/SwadlokApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swadlok;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwadlokApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
